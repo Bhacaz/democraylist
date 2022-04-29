90b9abf77c076616690e9f8bed1063c8bf8e126c
Jean-Francis Bastien <bhacaz@gmail.com>
2022-04-29 14:09:30 -0400

    Reduce js

--- db/schema.rb
+++ db/schema.rb
@@ -2,22 +2,21 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_010919) do
-
+ActiveRecord::Schema[7.0].define(version: 2020_06_05_010919) do
   create_table "join_playlist_invites", force: :cascade do |t|
     t.integer "invited_by_id"
     t.integer "user_id"
     t.integer "playlist_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["invited_by_id"], name: "index_join_playlist_invites_on_invited_by_id"
     t.index ["playlist_id"], name: "index_join_playlist_invites_on_playlist_id"
     t.index ["user_id"], name: "index_join_playlist_invites_on_user_id"
@@ -28,8 +27,8 @@ ActiveRecord::Schema.define(version: 2020_06_05_010919) do
     t.string "description"
     t.string "name"
     t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "song_size"
     t.integer "share_setting", default: 0
     t.index ["user_id"], name: "index_playlists_on_user_id"
@@ -44,8 +43,8 @@ ActiveRecord::Schema.define(version: 2020_06_05_010919) do
   create_table "subscriptions", force: :cascade do |t|
     t.integer "user_id"
     t.integer "playlist_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["playlist_id"], name: "index_subscriptions_on_playlist_id"
     t.index ["user_id", "playlist_id"], name: "index_subscriptions_on_user_id_and_playlist_id", unique: true
     t.index ["user_id"], name: "index_subscriptions_on_user_id"
@@ -55,8 +54,8 @@ ActiveRecord::Schema.define(version: 2020_06_05_010919) do
     t.string "spotify_id"
     t.integer "playlist_id"
     t.integer "added_by_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["added_by_id"], name: "index_tracks_on_added_by_id"
     t.index ["playlist_id"], name: "index_tracks_on_playlist_id"
   end
@@ -65,8 +64,8 @@ ActiveRecord::Schema.define(version: 2020_06_05_010919) do
     t.string "name"
     t.string "email"
     t.string "spotify_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "access_token"
     t.integer "expires_at"
     t.string "refresh_token"
@@ -77,8 +76,8 @@ ActiveRecord::Schema.define(version: 2020_06_05_010919) do
     t.integer "track_id"
     t.integer "user_id"
     t.integer "vote"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["track_id"], name: "index_votes_on_track_id"
     t.index ["user_id"], name: "index_votes_on_user_id"
   end
