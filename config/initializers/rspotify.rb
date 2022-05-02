221edd826311398404792721f25cd65d1bb02718
Jean-Francis Bastien <bhacaz@gmail.com>
2022-05-02 08:10:23 -0400

    compile without secrets

--- config/initializers/rspotify.rb
+++ config/initializers/rspotify.rb
@@ -1,4 +1,6 @@
 
+return unless ENV['spotify_client_id']
+
 RSpotify::authenticate(ENV['spotify_client_id'], ENV['spotify_client_secret'])
 
 require_relative '../../app/lib/r_spotify/track_extension'
