0cdff91aa4a0126b220b131bbd0f15b2cb7e0dab
Jean-Francis Bastien <jfbastien@petalmd.com>
2023-06-03 14:27:22 -0400

    Dynamic search works

--- app/controllers/home_turbo_controller.rb
+++ app/controllers/home_turbo_controller.rb
@@ -1,10 +1,23 @@
+# frozen_string_literal: true
+
 class HomeTurboController < ApplicationController
-  layout "application_turbo"
+  layout 'application_turbo'
 
   def index
-    @playlists = Playlist.all
+    @playlists =
+      if params[:query].present?
+        Playlist.where('name LIKE ?', "%#{params[:query]}%")
+      else
+        Playlist.all
+      end
     @menu_items = MenuHelper.items(
       ['logout', 'Logout', auth_logout_path]
     )
+
+    respond_to do |format|
+      format.html do |variant|
+        variant.turbo_frame { render partial: 'playlists' }
+      end
+    end
   end
 end
